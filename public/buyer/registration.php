1fd3df32bc53b087a8e23664ade0ba2de0adc2db
Agrishop <hlbctagumbranch1@gmail.com>
2025-01-05 18:23:59 +0800

    Randel Updates

--- public/buyer/registration.php
+++ public/buyer/registration.php
@@ -3,7 +3,7 @@
 <?php
 $statement = $pdo->prepare("SELECT * FROM tbl_settings WHERE id=1");
 $statement->execute();
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $row) {
     $banner_registration = $row['banner_registration'];
 }
@@ -29,7 +29,7 @@ if (isset($_POST['form1'])) {
         } else {
             $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_email=?");
             $statement->execute(array($_POST['cust_email']));
-            $total = $statement->rowCount();                            
+            $total = $statement->rowCount();
             if($total) {
                 $valid = 0;
                 $error_message .= LANG_VALUE_147."<br>";
@@ -153,7 +153,7 @@ if (isset($_POST['form1'])) {
 
         // Send email for confirmation of the account
         $to = $_POST['cust_email'];
-        
+
         $subject = LANG_VALUE_150;
         $verify_link = BASE_URL.'verify.php?email='.$to.'&token='.$token;
         $message = '
@@ -163,10 +163,10 @@ if (isset($_POST['form1'])) {
 
         $headers = "From: noreply@" . BASE_URL . "\r\n" .
                    "Reply-To: noreply@" . BASE_URL . "\r\n" .
-                   "X-Mailer: PHP/" . phpversion() . "\r\n" . 
-                   "MIME-Version: 1.0\r\n" . 
+                   "X-Mailer: PHP/" . phpversion() . "\r\n" .
+                   "MIME-Version: 1.0\r\n" .
                    "Content-Type: text/html; charset=ISO-8859-1\r\n";
-        
+
         // Sending Email
         mail($to, $subject, $message, $headers);
 
@@ -186,7 +186,7 @@ if (isset($_POST['form1'])) {
 
 <div class="page-banner" style="background-color:#444;background-image: url(assets/uploads/<?php echo $banner_registration; ?>);">
     <div class="inner">
-        <h1><?php echo LANG_VALUE_16; ?></h1>
+        <h1>Registration</h1>
     </div>
 </div>
 
@@ -196,14 +196,14 @@ if (isset($_POST['form1'])) {
             <div class="col-md-12">
                 <div class="user-content">
 
-                    
+
 
                     <form action="" method="post">
                         <?php $csrf->echoInputField(); ?>
                         <div class="row">
                             <div class="col-md-2"></div>
                             <div class="col-md-8">
-                                
+
                                 <?php
                                 if($error_message != '') {
                                     echo "<div class='error' style='padding: 10px;background:#f1f1f1;margin-bottom:20px;'>".$error_message."</div>";
@@ -214,70 +214,70 @@ if (isset($_POST['form1'])) {
                                 ?>
 
                                 <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_102; ?> *</label>
+                                    <label for="">First Name *</label>
                                     <input type="text" class="form-control" name="cust_name" value="<?php if(isset($_POST['cust_name'])){echo $_POST['cust_name'];} ?>">
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_103; ?></label>
+                                    <label for="">Last Name *</label>
                                     <input type="text" class="form-control" name="cust_cname" value="<?php if(isset($_POST['cust_cname'])){echo $_POST['cust_cname'];} ?>">
+                                    <label for="">Middle Initial *</label>
+                                    <input type="text" class="form-control" name="cust_cname" value="<?php if(isset($_POST['cust_cname'])){echo $_POST['cust_cname'];} ?>">
+                                </div>
+
+                                <div class="col-md-6 form-group">
+                                    <label for="">Telephone number *</label>
+                                    <input type="text" class="form-control" name="cust_cname" value="<?php if(isset($_POST['cust_cname'])){echo $_POST['cust_cname'];} ?>">
+                                </div>
+
+                                <div class="col-md-6 form-group">
+                                    <label for=""><?php echo LANG_VALUE_104; ?> *</label>
+                                    <input type="text" class="form-control" name="cust_phone" value="<?php if(isset($_POST['cust_phone'])){echo $_POST['cust_phone'];} ?>">
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for=""><?php echo LANG_VALUE_94; ?> *</label>
                                     <input type="email" class="form-control" name="cust_email" value="<?php if(isset($_POST['cust_email'])){echo $_POST['cust_email'];} ?>">
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_104; ?> *</label>
-                                    <input type="text" class="form-control" name="cust_phone" value="<?php if(isset($_POST['cust_phone'])){echo $_POST['cust_phone'];} ?>">
+                                    <label for="">Date of birth *</label>
+                                    <input type="date" class="form-control" name="cust_email" value="<?php if(isset($_POST['cust_email'])){echo $_POST['cust_email'];} ?>">
                                 </div>
                                 <div class="col-md-12 form-group">
                                     <label for=""><?php echo LANG_VALUE_105; ?> *</label>
                                     <textarea name="cust_address" class="form-control" cols="30" rows="10" style="height:70px;"><?php if(isset($_POST['cust_address'])){echo $_POST['cust_address'];} ?></textarea>
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_106; ?> *</label>
-                                    <select name="cust_country" class="form-control select2">
-                                        <option value="">Select country</option>
-                                    <?php
-                                    $statement = $pdo->prepare("SELECT * FROM tbl_country ORDER BY country_name ASC");
-                                    $statement->execute();
-                                    $result = $statement->fetchAll(PDO::FETCH_ASSOC);                            
-                                    foreach ($result as $row) {
-                                        ?>
-                                        <option value="<?php echo $row['country_id']; ?>"><?php echo $row['country_name']; ?></option>
-                                        <?php
-                                    }
-                                    ?>    
-                                    </select>                                    
-                                </div>
-                                
-                                <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_107; ?> *</label>
-                                    <input type="text" class="form-control" name="cust_city" value="<?php if(isset($_POST['cust_city'])){echo $_POST['cust_city'];} ?>">
-                                </div>
-                                <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_108; ?> *</label>
-                                    <input type="text" class="form-control" name="cust_state" value="<?php if(isset($_POST['cust_state'])){echo $_POST['cust_state'];} ?>">
-                                </div>
-                                <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_109; ?> *</label>
-                                    <input type="text" class="form-control" name="cust_zip" value="<?php if(isset($_POST['cust_zip'])){echo $_POST['cust_zip'];} ?>">
-                                </div>
-                                <div class="col-md-6 form-group">
                                     <label for=""><?php echo LANG_VALUE_96; ?> *</label>
                                     <input type="password" class="form-control" name="cust_password">
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for=""><?php echo LANG_VALUE_98; ?> *</label>
+                                    <label for="">Confirm Password *</label>
                                     <input type="password" class="form-control" name="cust_re_password">
+                                </div>
+                                <div class="col-md-6 form-group">
+                                    <label for="">Upload Supporting Information *</label>
+                                    <input type="file" class="form-control" name="cust_re_password">
+                                </div>
+                                <div class="col-md-6 form-group">
+                                    <label for="">Upload QR Code *</label>
+                                    <input type="file" class="form-control" name="cust_re_password">
+                                </div>
+
+
+                                <div style=" color: #ff0000;"class="col-md-6 form-group">
+                                <label for="">* If You are a Buyer provide your Valid ID</label>   <br>
+                                <label for="">* If You are a Seller provide your Business Permit</label><br>
+                                <label for="">* If You are a Seller provide your QR Code</label>
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for=""></label>
                                     <input type="submit" class="btn btn-danger" value="<?php echo LANG_VALUE_15; ?>" name="form1">
                                 </div>
+
+
                             </div>
-                        </div>                        
+                        </div>
                     </form>
-                </div>                
+                </div>
             </div>
         </div>
     </div>
